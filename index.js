dd2959c019291547c1b3daaa94a429587ab56450
Tim <timtran007@gmail.com>
2022-09-26 04:15:05 -0500

    commit tree f599425d595fba5f3192346bcfa6f214a94e284f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record){
+        let findResults = record.find(findWin)
+        if(!!findResults === true){
+            return findResults.year
+        } else{
+            return undefined
+        }
+    }
+
+function findWin(record){
+return record.result === "W"
+}
